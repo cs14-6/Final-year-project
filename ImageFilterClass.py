e928a3a24e226a90dc9d632487262e5c0d498b13
lloyd <spnlloyd@gmail.com>
2014-04-29 08:07:04 +0300

    image filtering module

--- ImageFilterClass.py
+++ ImageFilterClass.py
@@ -14,12 +14,7 @@ def read_and_clean_image(path):
     img = cv2.imread(path)
 
     DELAY_CAPTION = 15000
-
-    for i in xrange(1, 10, 2):
-        gaussian_blur = cv2.GaussianBlur(img, (i, i), 0)
-        cv2.imshow('Blur', gaussian_blur)
-
-    cv2.waitKey(DELAY_CAPTION)
+    cv2.imshow('Normal', img)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
@@ -28,7 +23,7 @@ def read_and_clean_image(path):
     cv2.waitKey(DELAY_CAPTION)
 
 
-read_and_clean_image("skin141.png")
+read_and_clean_image('img_res/inf_skin11.png')
 
 #sift = cv2.SIFT()
 #kp = sift.detect(read_and_clean_image("O:/PROJECTS/SKIN/SKIN IMAGES/skin1.png"))
